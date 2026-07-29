df7cd48686a3afb8ef4abc57c9c314225e69c63f
sanneiphyo-portedi <sanneiphyo@gmail.com>
2026-07-29 20:41:42 +0630

    feat : Dapper CRUD

--- ConsoleApp4/DapperService.cs
+++ ConsoleApp4/DapperService.cs
@@ -1,12 +1,100 @@
-﻿using System;
+﻿using Dapper;
+using Microsoft.Data.SqlClient;
+using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace June2026.ConsoleApp4
 {
-    internal class DapperService
+    public class DapperService
     {
+        private readonly SqlConnectionStringBuilder sb = new SqlConnectionStringBuilder
+        {
+            DataSource = ".", //(local) // server name
+            InitialCatalog = "June2026Db", // database name
+            UserID = "sa",
+            Password = "sasa@123",
+            TrustServerCertificate = true
+        };
+
+        public void Read()
+        {
+            using IDbConnection db = new SqlConnection(sb.ConnectionString);
+            db.Open();
+            List<StudentDto> lst = db.Query<StudentDto>("SELECT * FROM [dbo].[Tbl_Student];").ToList();
+            foreach (var item in lst)
+            {
+                Console.WriteLine($"Id: {item.StudentID}, Name: {item.StudentName}");
+            }
+            db.Close();
+
+        }
+        
+        public void Create()
+        {
+            using IDbConnection db = new SqlConnection(sb.ConnectionString);
+            db.Open();
+            int res = db.Execute(@"INSERT INTO Tbl_Student
+                            (
+                                StudentName,
+                                FatherName,
+                                StudentNo,
+                                Email,
+                                MobileNo,
+                                IsDelete
+                            )
+                            VALUES
+                            (
+                                'John Doe',
+                                'Michael Doe',
+                                'STU001',
+                                'john.doe@gmail.com',
+                                '09123456789',
+                                0
+                            );");
+            Console.WriteLine($"Rows Created: {res}");
+            db.Close();
+        }
+
+        public void Update()
+            {
+                    using IDbConnection db = new SqlConnection(sb.ConnectionString);
+                    db.Open();
+
+                    int res = db.Execute(@"
+                UPDATE Tbl_Student
+                SET
+                    StudentName = 'Jane Doe',
+                    FatherName = 'David Doe',
+                    StudentNo = 'STU001',
+                    Email = 'jane.doe@gmail.com',
+                    MobileNo = '09987654321',
+                    IsDelete = 0
+                WHERE StudentID = 5;
+            ");
+
+                    Console.WriteLine($"Rows Updated: {res}");
+
+                    db.Close();
+                }
+
+        public void Delete() { 
+        
+            using IDbConnection db = new SqlConnection( sb.ConnectionString);
+
+            int res = db.Execute(@"
+                DELETE FROM Tbl_Student
+                WHERE StudentID = 7;
+            ");
+
+            Console.WriteLine($"Rows Updated: {res}");
+        }
+    
+    
     }
-}
+
+
+};
--- ConsoleApp4/Program.cs
+++ ConsoleApp4/Program.cs
@@ -1,2 +1,29 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using June2026.ConsoleApp4;
+
 Console.WriteLine("Hello, World!");
+
+DapperService dapperService = new DapperService();
+
+//dapperService.Read();
+//dapperService.Create();
+//dapperService.Update();
+dapperService.Delete();
+
+
+public class StudentDto
+{
+    public int StudentID { get; set; }
+
+    public string? StudentName { get; set; }
+
+    public string? FatherName { get; set; }
+
+    public string? StudentNo { get; set; }
+
+    public string? Email { get; set; }
+
+    public string? MobileNo { get; set; }
+
+    public bool IsDelete { get; set; }
+}
