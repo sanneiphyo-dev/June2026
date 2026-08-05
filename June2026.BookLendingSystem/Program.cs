f16bb30ba9e725951bc126b30a450109dd2fadf1
sanneiphyo-portedi <sanneiphyo@gmail.com>
2026-08-06 00:27:10 +0630

    feat : BLS Create /Read

---
+++ June2026.BookLendingSystem/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+
+public class BookModel
+{
+    public int BookId { get; set; }
+    public string Title { get; set; } = string.Empty;
+    public string Author { get; set; } = string.Empty;
+    public string? Publisher { get; set; }
+    public string? Category { get; set; }
+}
